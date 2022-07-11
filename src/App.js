368ca0bcb2de7cda77c99b64717329e1b2f9bb95
Jerome-Joseph <jeromejoseph782@gmail.com>
2022-07-11 11:50:42 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const fname = useRef(null);
+  const lname = useRef(null);
+  const [names, setNames] = useState([])
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log(fname.current.value, lname.current.value);
+    let firstName = fname.current.value
+    let lastName = lname.current.value
+    setNames([...names, {id:names.length + 1, firstName, lastName}])
+    console.log(names);
+  }
+
+  // const sorted =(a,b)=>{
+  //   return b.id - a.id
+  // }
+  // names.sort(sorted);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3 mt-5">
+          <label htmlFor="exampleInputEmail1" className="form-label">Enter your first name</label>
+          <input type="text" className="form-control" ref={fname} id="firstname" name='firstname'  aria-describedby="emailHelp"/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Enter your Last name</label>
+          <input type="text" className="form-control" ref={lname} id="lastname" name='lastname'  aria-describedby="emailHelp"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+
+      <table className="table mt-3">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+          </tr>
+        </thead>
+        <tbody>
+          {names.map((name)=>{
+            return <tr key={name.id}>
+            <th scope="row">{name.id}</th>
+            <td>{name.firstName}</td>
+            <td>{name.lastName}</td>
+          </tr>
+          })}
+        </tbody>
+      </table>
+      {names.length === 0 && 'No users to display'}
     </div>
   );
 }
